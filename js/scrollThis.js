5ea6084d94fecc6837fb010ea6a27875f19c4425
Brian Noah <brianjoshuanoah@gmail.com>
2012-10-24 11:16:06 -0700

    working with the DOM

--- js/scrollThis.js
+++ js/scrollThis.js
@@ -1,39 +1,39 @@
-$(document).ready(function() {
-	var scrollThis = function(cont) {
-		cont.on('hover',function() {
-			var move_this = cont.children();
-			var height = cont.children().innerHeight();
-			var cont_height = cont.innerHeight();
-			var delta_d = move_this.position().top;
-			var max_scroll = (height - cont_height)*(-1);
-			//console.log(height, cont_height, delta_d, max_scroll);
-			cont.on('mousewheel', function(e, d){
-				console.log(e, d);
-				if (d > 1){
-					d = 1;
-				}
-				delta_d = delta_d + d*10;
-				if (delta_d < max_scroll){
-					delta_d = max_scroll;
-					move_this.css({
-						"top": delta_d + 'px',
-					});
-				} else if (delta_d > 0){
-					delta_d = 0;
-					move_this.css({
-						"top": delta_d + 'px',
-					});
-				}
+var scrollThis = function(cont) {
+	cont.on('hover', function() {
+		var move_this = cont.children(':first');
+		var height = move_this.innerHeight();
+		var cont_height = cont.innerHeight();
+		var delta_d = move_this.position().top;
+		var max_scroll = (height - cont_height)*(-1);
+		cont.on('scroll', function(e,d){
+			console.log(e);
+			if (d > 1){
+				d = 1;
+			}
+			delta_d = delta_d + d*10;
+			//console.log(delta_d);
+			if (delta_d < max_scroll){
+				delta_d = max_scroll;
 				move_this.css({
-					'top': delta_d + "px",
-				},0); 
-				e.preventDefault();
-			});
-			return false;
+					"top": delta_d + 'px',
+				});
+			} else if (delta_d > 0){
+				delta_d = 0
+				move_this.css({
+					"top": delta_d + 'px',
+				});
+			}
+			move_this.css({
+				'top': delta_d + "px",
+			},0); 
+			e.preventDefault();
 		});
-	}
+	});
+	return false;
+}
 
-		
+$(document).ready(function() {
+
 	scrollThis($('#container'));
 
 });
