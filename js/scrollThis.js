4c3639517d29c1762bf4e2205aaacb574bd508e6
Brian Noah <brianjoshuanoah@gmail.com>
2012-10-24 15:02:16 -0700

    added jquery.mousewheel

--- js/scrollThis.js
+++ js/scrollThis.js
@@ -1,17 +1,19 @@
-var scrollThis = function(cont) {
-	cont.on('hover', function() {
+var scrollThis = function(cont){
+	var start, end;
+	cont.on('hover',function(){
 		var move_this = cont.children(':first');
 		var height = move_this.innerHeight();
 		var cont_height = cont.innerHeight();
 		var delta_d = move_this.position().top;
 		var max_scroll = (height - cont_height)*(-1);
-		cont.on('scroll', function(e,d){
-			console.log(e);
+		var moov = 0;
+		cont.on('mousewheel', function(e,d){
+			e.preventDefault();
 			if (d > 1){
 				d = 1;
 			}
 			delta_d = delta_d + d*10;
-			//console.log(delta_d);
+			// console.log(delta_d);
 			if (delta_d < max_scroll){
 				delta_d = max_scroll;
 				move_this.css({
@@ -26,10 +28,46 @@ var scrollThis = function(cont) {
 			move_this.css({
 				'top': delta_d + "px",
 			},0); 
-			e.preventDefault();
+			
 		});
+		cont.on('touchstart', function(e){
+			e.preventDefault();
+			var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+			 var start = touch.pageY;
+			 cont.on('touchmove', function(e){
+				e.preventDefault();
+				var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+				 
+				 var move = touch.pageY;
+				 var d = (start - move) * -1;
+				 //console.log(d, start);
+
+				if (d > 1){
+					d = 1;
+				} else if (d < -1){
+					d = -1;
+				}
+				delta_d = delta_d + d*5;
+				// console.log(delta_d);
+				if (delta_d < max_scroll){
+					delta_d = max_scroll;
+					move_this.css({
+						"top": delta_d + 'px',
+					});
+				} else if (delta_d > 0){
+					delta_d = 0
+					move_this.css({
+						"top": delta_d + 'px',
+					});
+				}
+				move_this.css({
+					'top': delta_d + "px",
+				},0);
+				return move;
+			});
+		});
+		return false;
 	});
-	return false;
 }
 
 $(document).ready(function() {
