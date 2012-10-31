0ecacc7e1c20b02b16ab9c11ac102e849204ce15
Brian Noah <brianjoshuanoah@gmail.com>
2012-10-31 16:10:59 -0700

    Added scrollThisToEnd() and slight bug fixes

--- js/scrollThis.js
+++ js/scrollThis.js
@@ -1,29 +1,35 @@
-var scrollThis = function(cont){
-	var start, end;
-	cont.hover(function(){
-		cont.addClass('scroll_time');
-		var move_this = cont.children(':first');
-		var height = move_this.innerHeight();
-		var cont_height = cont.innerHeight();
-		var delta_d = move_this.position().top;
-		var max_scroll = (height - cont_height) * (-1);
-		var scroller_pct = (cont_height/height) * 100;
-		var scroller_height = (cont_height/height) * cont_height;
-		var max_scroller_scroll = (cont_height - scroller_height);
+var scrollThis = function (container) {
+	var start, end, move_this, height, container_height, delta_d, max_scroll, scroller_pct, scroller_height, max_scroller_scroll, scroller;
+	if (container.children().length < 2){
+		container.append('<div id="scrollbar_container"><div class="scroll_bar" style=""></div></div>');
+		container.after('<style>#scrollbar_container{top:0; right:4px; width:7px; position:absolute; height:100%;} .scroll_bar{top:0; width:7px; border-radius:3px; height:0px; background: #999;opacity:0;cursor:pointer; -webkit-transition: opacity .8s linear; -moz-transition: opacity .8s linear; -o-transition: opacity .8s linear; transition: opacity .8s linear;} .scroll_time .scroll_bar{opacity:1} .scroll_bar:active{background:#777;}</style>');
+	}
+	container.hover(function () {
+		container.addClass('scroll_time');
+		move_this = container.children(':first');
+		height = move_this.innerHeight();
+		container_height = container.innerHeight();
+		delta_d = move_this.position().top;
+		max_scroll = (height - container_height) * (-1);
+		scroller_pct = (container_height/height) * 100;
+		scroller_height = (container_height/height) * container_height;
+		max_scroller_scroll = (container_height - scroller_height);
 		//console.log(scroller_height, max_scroller_scroll)
-		var scroller = $('.scroll_bar', cont);
-		scroller.css({"height": "" + scroller_pct + "%"});
-		cont.on('mousewheel', function(e,d){
+		scroller = $('.scroll_bar', container);
+		scroller.css({
+			"height": "" + scroller_pct + "%"
+		});
+		container.on('mousewheel', function (e,d) {
 			e.preventDefault();
 			if (d > 1){d = 1;}
 			delta_d = delta_d + d*10;
 			// console.log(delta_d);
-			if (delta_d < max_scroll){
+			if (delta_d < max_scroll) {
 				delta_d = max_scroll;
 				move_this.css({
 					"top": delta_d + 'px',
 				});
-			} else if (delta_d > 0){
+			} else if (delta_d > 0) {
 				delta_d = 0
 				move_this.css({
 					"top": delta_d + 'px',
@@ -31,9 +37,9 @@ var scrollThis = function(cont){
 			}
 			move_this.css({
 				'top': delta_d + "px",
-			},0); 
+			}); 
 			scroller.css({
-				'top': (delta_d/max_scroll)*max_scroller_scroll + "px",
+				'top': (delta_d / max_scroll) * max_scroller_scroll + "px",
 			});
 
 		});
@@ -41,30 +47,55 @@ var scrollThis = function(cont){
 			axis: "y",
 			scroll: true,
 			containment: "parent", 
-			drag: function(e, f){
+			drag: function (e, f) {
 				var top = f.position.top;
 				move_this.css({
-					'top': (top * max_scroll)/max_scroller_scroll + "px",
+					'top': (top * max_scroll) / max_scroller_scroll + "px",
 				}); 
 			},
-			stop: function(){
-				setTimeout(function(){
-					if (!cont.hasClass('scroll_time')){
-						cont.addClass('scroll_time');
+			stop: function () {
+				setTimeout(function () {
+					if (!container.hasClass('scroll_time')){
+						container.addClass('scroll_time');
 					}
-				},1);
+				}, 1);
 			}
 		});
 		return false;
-	}, function() {
-		cont.removeClass('scroll_time');
+	}, function () {
+		container.removeClass('scroll_time');
 	});
 	var platform = window.clientInformation.platform;
 	var plt = platform.toLowerCase();
-	if (plt == 'ipad' || plt == 'ipod' || plt == 'iphone' || plt.indexOf('arm') > -1 || plt == 'blackberry')
-	{
-		cont.css({'overflow':'auto'});
+	if (plt == 'ipad' || plt == 'ipod' || plt == 'iphone' || plt.indexOf('arm') > -1 || plt == 'blackberry') {
+		container.css({'overflow':'auto'});
 	}
-	cont.append('<div id="scrollbar_container"><div class="scroll_bar" style=""></div></div>')
-	cont.after('<style>#scrollbar_container{top:0; right:4px; width:7px; position:absolute; height:100%;} .scroll_bar{top:0; width:7px; border-radius:3px; height:0px; background: #999;opacity:0;cursor:pointer; -webkit-transition: opacity .8s linear; -moz-transition: opacity .8s linear; -o-transition: opacity .8s linear; transition: opacity .8s linear;} .scroll_time .scroll_bar{opacity:1} .scroll_bar:active{background:#777;}</style>')
 }
+
+var scrollThisToEnd = function (id) {
+	var move_this, height, container_height, max_scroll, scroller_pct, scroller_height, max_scroller_scroll, scroller;
+
+	var this_id = "message_"+id;
+   	container = $("#" + this_id);
+   	//console.log(this_id);
+   	setTimeout(function () {
+   		scrollThis(container);
+		move_this = container.children(':first');
+		height = move_this.innerHeight();
+		container_height = container.innerHeight();
+		max_scroll = (height - container_height) * (-1);
+		scroller_pct = (container_height/height) * 100;
+		scroller_height = (container_height/height) * container_height;
+		max_scroller_scroll = (container_height - scroller_height);
+		scroller = $('.scroll_bar', container);
+		scroller.css({
+			"height": "" + scroller_pct + "%"
+		});
+		move_this.animate({
+			top: max_scroll + 'px',
+		});
+		scroller.animate({
+			'top': max_scroller_scroll + "px",
+		});
+    }, 500);
+}	
